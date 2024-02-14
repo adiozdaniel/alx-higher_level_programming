afa9d7576d179ba5fed5e4e945a6011fda5acc96
ADIOZ DANIEL <adiozdaniel@gmail.com>
2024-02-14 07:38:59 +0300

    15 groups

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,8 @@
 -- This lists all records of the table second_table
 -- (having a name value in my MySQL server)
 -- Records are ordered by descending score
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `name` != ""
-ORDER BY `score` DESC
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
+
